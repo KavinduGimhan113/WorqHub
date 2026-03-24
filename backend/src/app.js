91781261403b16792d1772052b0bd50d3ea84d2f
Kavindu Gimhan <kavindugimhan113@gmail.com>
2026-03-24 16:17:32 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type","Authorization"],
   credentials: true
 }));
-app.options('*', cors());
+app.options(/.*/, cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
