2668fdee55f74cb283ceee30007dbe90531b6821
Kavindu Gimhan <kavindugimhan113@gmail.com>
2026-04-22 10:56:44 +0530

    Update inventory.js

--- frontend/src/api/inventory.js
+++ frontend/src/api/inventory.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 /**
  * Inventory API. All calls include JWT; backend scopes by tenantId.
  */
@@ -20,15 +19,4 @@ export const createCategory = (name) =>
   client.post('/inventory/categories', { name }).then((res) => res.data);
 export const removeCategory = (id) =>
   client.delete(`/inventory/categories/${id}`).then((res) => res.data);
-=======
-/**
- * Inventory API. All calls include JWT; backend scopes by tenantId.
- */
-import client from './client';
 
-export const list = (params) => client.get('/inventory', { params }).then((res) => res.data);
-export const get = (id) => client.get(`/inventory/${id}`).then((res) => res.data);
-export const create = (data) => client.post('/inventory', data).then((res) => res.data);
-export const update = (id, data) => client.put(`/inventory/${id}`, data).then((res) => res.data);
-export const remove = (id) => client.delete(`/inventory/${id}`).then((res) => res.data);
->>>>>>> Stashed changes
